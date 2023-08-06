70c1e947a98b3d1bec92b9df8b79319470201d1b
CristianRG <cristianrizogomez890@gmail.com>
2023-08-06 17:02:03 -0600

    deleted

--- Central/web/js/changeModule.js
+++
@@ -1,9 +0,0 @@
-async function chargeSucursalModule(){
-    let content = fetch('./sucursal.html');
-    document.getElementById('content').innerHTML = await content.text();
-}
-
-async function chargeProductModule(){
-    let content = fetch('./productos.html');
-    document.getElementById('content').innerHTML = await content.text();
-}
