c8eeb83fe74a15f85c25484b595cfd5864fcd061
CristianRG <cristianrizogomez890@gmail.com>
2023-08-05 19:56:27 -0600

    example

--- Sucursal/web/js/changeModule.js
+++ Sucursal/web/js/changeModule.js
@@ -1,24 +1,25 @@
 async function chargeEmployeeModule(){
-    let content = fetch('../empleado.html');
-    document.getElementById('content').innerHTML = (await content).text;
+    let content = fetch('./empleado.html');
+    document.getElementById('content').innerHTML = await content.text();
 }
 
 async function chargeClientModule(){
-    let content = fetch('../cliente.html');
-    document.getElementById('content').innerHTML = (await content).text;
+    let content = fetch('./cliente.html');
+    document.getElementById('content').innerHTML = await content.text();
 }
 
 async function chargeProductModule(){
-    let content = fetch('../productos.html');
-    document.getElementById('content').innerHTML = (await content).text;
+    let content = fetch('./productos.html');
+    document.getElementById('content').innerHTML = await content.text();
 }
 
 async function chargeSaleModule(){
-    let content = fetch('../venta.html');
-    document.getElementById('content').innerHTML = (await content).text;
+    let content = fetch('./venta.html');
+    document.getElementById('content').innerHTML = await content.text();
 }
 
 async function chargeOrderModule(){
-    let content = fetch('../pedido.html');
-    document.getElementById('content').innerHTML = (await content).text;
+    let module = await fetch('./pedidos.html');
+    console.log(module)
+    document.getElementById('content').innerHTML = await module.text();
 }
