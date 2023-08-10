106014099e838117e19625f642c9c723e0e4bec5
Josue <josuechavez0312@gmail.com>
2023-08-10 00:27:25 -0600

    Mugre sicefa

--- Central/web/js/adProducto.js
+++ Central/web/js/adProducto.js
@@ -1,120 +1,120 @@
-       let indexProductosSeleccionado;
-  const productos = [
-        {
-            "nombre": "Pain Relief plus",
-            "nombreGenerico": "Analgesinol",
-            "formaFarmaceutica": "Tabletas",
-            "medida": "500 mg",
-            "presentación": "Caja de 20 tabletas",
-            "concentración": "250 mg por tableta",
-            "unidadesEenvase": "20 tabletas",
-            "precio": "$12.99",
-            "estatus": "Activo"
-        },
+let indexProductosSeleccionado;
+const productos = [
+    {
+        "nombre": "Pain Relief plus",
+        "nombreGenerico": "Analgesinol",
+        "formaFarmaceutica": "Tabletas",
+        "medida": "500 mg",
+        "presentación": "Caja de 20 tabletas",
+        "concentración": "250 mg por tableta",
+        "unidadesEenvase": "20 tabletas",
+        "precio": "$12.99",
+        "estatus": "Activo"
+    },
 
-        {
-                "nombre": "AllerGuard Syrup",
-                "nombreGenerico": "Antihistaminol",
-                "formaFarmaceutica": "Jarabe",
-                "medida": "120 ml",
-                "presentación": "Botella de 120 ml",
-                "concentración": "5 mg por 5 ml",
-                "unidadesEenvase": "1 botella",
-                "precio": "$8.49",
-                "estatus": "Activo"
-            },
+    {
+        "nombre": "AllerGuard Syrup",
+        "nombreGenerico": "Antihistaminol",
+        "formaFarmaceutica": "Jarabe",
+        "medida": "120 ml",
+        "presentación": "Botella de 120 ml",
+        "concentración": "5 mg por 5 ml",
+        "unidadesEenvase": "1 botella",
+        "precio": "$8.49",
+        "estatus": "Activo"
+    },
 
-        {
-            "nombre": "VitaC Boost",
-            "nombreGenerico": "Vitamina C",
-            "formaFarmaceutica": "Cápsulas",
-            "medida": "1000 mg",
-            "presentación": "Frasco de 60 cápsulas",
-            "concentración": "500 mg por cápsula",
-            "unidadesEenvase": "60 cápsulas",
-            "precio": "$15.75",
-            "estatus": "Activo"
-        },
+    {
+        "nombre": "VitaC Boost",
+        "nombreGenerico": "Vitamina C",
+        "formaFarmaceutica": "Cápsulas",
+        "medida": "1000 mg",
+        "presentación": "Frasco de 60 cápsulas",
+        "concentración": "500 mg por cápsula",
+        "unidadesEenvase": "60 cápsulas",
+        "precio": "$15.75",
+        "estatus": "Activo"
+    },
 
-        {
-           
-    "nombre": "SleepEase Tablets",
-    "nombreGenerico": "Somniol",
-    "formaFarmaceutica": "Tabletas",
-    "medida": "5 mg",
-    "presentación": "Caja de 30 tabletas",
-    "Concentración": "5 mg por tableta",
-    "unidadesEenvase": "30 tabletas",
-    "precio": "$9.99",
-    "estatus": "Activo"
-},
-{
-    "nombre": "DigestiPro Capsules",
-    "nombreGenerico": "Digestinol",
-    "formaFarmaceutica": "Cápsulas",
-    "medida": "300 mg",
-    "presentación": "Frasco de 90 cápsulas",
-    "concentración": "150 mg por cápsula",
-    "unidadesEnvase": "90 cápsulas",
-    "precio": "$18.50",
-     "estatus": "Activo"
-},
-{
-    "nombre": "ImmunoGuard Plus",
-    "nombreGenerico": "Inmunol",
-    "formaFarmaceutica": "Tabletas",
-    "medida": "750 mg",
-    "presentación": "Caja de 60 tabletas",
-    "concentración": "250 mg por tableta",
-    "unidadesEnvase": "60 tabletas",
-    "precio": "$22.99",
-    "estatus": "Activo"
-},
-{
-    "nombre": "RelaxiCalm Drops",
-    "nombreGenerico": "Calmex",
-    "formaFarmaceutica": "Gotas",
-    "medida": "10 ml",
-    "presentación": "Frasco de 10 ml",
-    "concentración": "5 mg por ml",
-    "unidadesEnvase": "1 frasco",
-    "precio": "$11.25",
-    "estatus": "Activo"
-},
-{
-    "nombre": "SkinCare Cream",
-    "nombreGenerico": "Dermatol",
-    "formaFarmaceutica": "Crema",
-    "medida": "30 g",
-    "presentación": "Tubo de 30 g",
-    "concentración": "1%",
-    "unidadesEnvase": "1 tubo",
-    "precio": "$14.75",
-    "estatus": "Activo"
-},
-{
-    "nombre": "JointFlex Gel",
-    "nombreGenerico": "Flexijoint",
-    "formaFarmaceutica": "Gel",
-    "medida": "50 g",
-    "presentación": "Tubo de 50 g",
-    "concentración": "2%",
-    "unidadesEnvase": "1 tubo",
-    "precio": "$16.99",
-    "estatus": "Activo"
-},
-{
-    "nombre": "EyeClear Drops",
-    "nombreGenerico": "VisiClear",
-    "formaFarmaceutica": "Gotas",
-    "medida": "15 ml",
-    "presentación": "Frasco de 15 ml",
-    "concentración": "2 mg por ml",
-    "unidadesEnvase": "1 frasco",
-    "precio": "$9.95",
-    "estatus": "Activo"
-}
-    ];
+    {
+
+        "nombre": "SleepEase Tablets",
+        "nombreGenerico": "Somniol",
+        "formaFarmaceutica": "Tabletas",
+        "medida": "5 mg",
+        "presentación": "Caja de 30 tabletas",
+        "Concentración": "5 mg por tableta",
+        "unidadesEenvase": "30 tabletas",
+        "precio": "$9.99",
+        "estatus": "Activo"
+    },
+    {
+        "nombre": "DigestiPro Capsules",
+        "nombreGenerico": "Digestinol",
+        "formaFarmaceutica": "Cápsulas",
+        "medida": "300 mg",
+        "presentación": "Frasco de 90 cápsulas",
+        "concentración": "150 mg por cápsula",
+        "unidadesEnvase": "90 cápsulas",
+        "precio": "$18.50",
+        "estatus": "Activo"
+    },
+    {
+        "nombre": "ImmunoGuard Plus",
+        "nombreGenerico": "Inmunol",
+        "formaFarmaceutica": "Tabletas",
+        "medida": "750 mg",
+        "presentación": "Caja de 60 tabletas",
+        "concentración": "250 mg por tableta",
+        "unidadesEnvase": "60 tabletas",
+        "precio": "$22.99",
+        "estatus": "Activo"
+    },
+    {
+        "nombre": "RelaxiCalm Drops",
+        "nombreGenerico": "Calmex",
+        "formaFarmaceutica": "Gotas",
+        "medida": "10 ml",
+        "presentación": "Frasco de 10 ml",
+        "concentración": "5 mg por ml",
+        "unidadesEnvase": "1 frasco",
+        "precio": "$11.25",
+        "estatus": "Activo"
+    },
+    {
+        "nombre": "SkinCare Cream",
+        "nombreGenerico": "Dermatol",
+        "formaFarmaceutica": "Crema",
+        "medida": "30 g",
+        "presentación": "Tubo de 30 g",
+        "concentración": "1%",
+        "unidadesEnvase": "1 tubo",
+        "precio": "$14.75",
+        "estatus": "Activo"
+    },
+    {
+        "nombre": "JointFlex Gel",
+        "nombreGenerico": "Flexijoint",
+        "formaFarmaceutica": "Gel",
+        "medida": "50 g",
+        "presentación": "Tubo de 50 g",
+        "concentración": "2%",
+        "unidadesEnvase": "1 tubo",
+        "precio": "$16.99",
+        "estatus": "Activo"
+    },
+    {
+        "nombre": "EyeClear Drops",
+        "nombreGenerico": "VisiClear",
+        "formaFarmaceutica": "Gotas",
+        "medida": "15 ml",
+        "presentación": "Frasco de 15 ml",
+        "concentración": "2 mg por ml",
+        "unidadesEnvase": "1 frasco",
+        "precio": "$9.95",
+        "estatus": "Activo"
+    }
+];
 
 // // Función para manejar el evento de clic en la fila
 // function handleRowClick(event) {
@@ -143,17 +143,17 @@
 //     const tableBody = document.querySelector('tbody');
 //     tableBody.addEventListener('click', handleRowClick);
 
-    document.getElementById('fotoInput').addEventListener('change', function(event) {
-        const preview = document.getElementById('fotoPreview');
-        const file = event.target.files[0];
-        if (file) {
-            preview.style.display = 'block';
-            preview.src = URL.createObjectURL(file);
-        } else {
-            preview.style.display = 'none';
-            preview.src = '#';
-        }
-    });
+document.getElementById('fotoInput').addEventListener('change', function (event) {
+    const preview = document.getElementById('fotoPreview');
+    const file = event.target.files[0];
+    if (file) {
+        preview.style.display = 'block';
+        preview.src = URL.createObjectURL(file);
+    } else {
+        preview.style.display = 'none';
+        preview.src = '#';
+    }
+});
 
 // fetch("empleado/dataEmpleados.json")
 //         .then(response => {
@@ -174,7 +174,7 @@ function addProducto() {
             concentración,
             unidadesEnvase,
             precio,
-            estatus
+            estatus;
 
     nombre = document.getElementById("nombreProdcto").value;
     nombreGenerico = document.getElementById("nombreGenerico").value;
@@ -184,13 +184,13 @@ function addProducto() {
     concentración = document.getElementById("concentración").value;
     unidadesEnvase = document.getElementById("unidadesEnvase").value;
     precio = document.getElementById("precio").value;
-    
+
     estatus = document.getElementById("estatus").value;
     estatusSelected = "";
 
     for (let i = 0; i < estatus.length; i++) {
         if (estatus[i].checked) {
-            generoSelected = estatus[i].value;
+            estatusSelected = estatus[i].value;
             break;
         }
     }
@@ -214,9 +214,9 @@ function addProducto() {
     producto.concentración = concentración;
     producto.unidadesEnvase = unidadesEnvase;
     producto.precio = precio;
-    // empleado.sucursal = sucursal;
+  
     producto.estatus = "Activo";
-    producto.push(producto);
+    productos.push(producto);
     clean();
     loadTabla();
 }
@@ -225,13 +225,13 @@ function loadTabla() {
     let cuerpo = "";
     productos.forEach(function (producto) {
         let registro =
-                '<tr onclick="selectEmpleado(' + productos.indexOf(empleado) + ');">' +
+                '<tr onclick="selectProducto(' + productos.indexOf(producto) + ');">' +
                 '<td>' + producto.nombreProducto + '</td>' +
                 '<td>' + producto.nombreGenerico + '</td>' +
                 '<td>' + producto.formaFarmaceutica + '</td>' +
                 '<td>' + producto.medida + '</td>' +
                 '<td>' + producto.presentación + '</td>' +
-                 '<td>' + producto.concentración + '</td>' +
+                '<td>' + producto.concentración + '</td>' +
                 '<td>' + producto.unidadesEnvase + '</td>' +
                 '<td>' + producto.estatus + '</td></tr>';
         cuerpo += registro;
@@ -241,7 +241,7 @@ function loadTabla() {
 
 }
 
-function selectproducto(index) {
+function selectProducto(index) {
     document.getElementById("nombreProducto").value = productos[index].nombre;
     document.getElementById("nombreGenerico").value = productos[index].nombreGenerico;
     document.getElementById("formaFarmaceutica").value = productos[index].formaFarmaceutica;
@@ -274,8 +274,8 @@ function clean() {
 }
 
 function updateProducto() {
-    let     
-   nombre,
+    let
+            nombre,
             nombreGenerico,
             formaFarmaceutica,
             medida,
@@ -291,21 +291,21 @@ function updateProducto() {
     medida = document.getElementById("medida").value;
     presentación = document.getElementById("presentación").value;
     concentración = document.getElementById("concentración").value;
-    
+
     estado = document.getElementById("unidadesEnvase").value;
     ciudad = document.getElementById("precio").value;
-    
+
     estatus = document.getElementById("estatus").value;
     estatusSelected = "";
 
     for (let i = 0; i < estatus.length; i++) {
-    if (estatus[i].checked) {
-        estatusSelected = estatus[i].value;
-        break;
-    }
+        if (estatus[i].checked) {
+            estatusSelected = estatus[i].value;
+            break;
+        }
     }
 
-    
+
 
     // sucursal = document.getElementById("txtSucursal").value;
     // sucursalSelected = "";
@@ -332,7 +332,7 @@ function updateProducto() {
     producto.precio = precio;
     // empleado.sucursal = sucursal;
     producto.estatus = "Activo";
-     productos[indexProductosSeleccionado] = empleado;
+    productos[indexProductosSeleccionado] = producto;
     producto.push(producto);
     clean();
     loadTabla();
@@ -351,12 +351,12 @@ document.getElementById("btnBuscar").addEventListener("click", searchProducto);
 function searchProducto() {
     const searchProd = document.getElementById("buscarProducto").value.toLowerCase();
     const filteredProducto = productos.filter(producto =>
-                producto.nombre.toLowerCase().includes(searchProd) ||
+        producto.nombre.toLowerCase().includes(searchProd) ||
                 producto.nombreGenerico.toLowerCase().includes(searchProd) ||
                 producto.formaFarmaceutica.toLowerCase().includes(searchProd) ||
                 // empleado.sucursal.toLowerCase().includes(searchEmp) ||
                 producto.presentación.toLowerCase().includes(searchProd) ||
-                producto.estatus.toLowerCase().includes(searchProd)    );
+                producto.estatus.toLowerCase().includes(searchProd));
 
     loadTablas(filteredProducto);
 }
@@ -365,7 +365,7 @@ function loadTablas(productosData) {
     const tablaBody = document.getElementById("tblProductos");
     let cuerpo = "";
 
-    empleadosData.forEach((producto, index) => {
+    productosData.forEach((producto, index) => {
         const registro = `
             <tr onclick="selectproducto(${index});">
                 <td>${producto.nombre}</td>
